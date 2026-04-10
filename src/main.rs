e62bf19802f129f79e3fad04410951f2412025d5
amida <aaaamida6747@gmail.com>
2026-04-10 14:15:48 +0800

    connect workers to app UI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use crate::ui::App;
+
 mod ui;
 mod rpc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+        use crate::rpc::worker::*;
+
         let native_opts = eframe::NativeOptions {
                 viewport: egui::ViewportBuilder::default()
                         .with_inner_size([800.0, 600.0])
@@ -15,12 +19,17 @@ async fn main() -> anyhow::Result<()> {
                 ..Default::default()
         };
 
-        rpc::Rpc::connect("ws://127.0.0.1:6800/jsonrpc").await?;
+        // rpc::Rpc::connect("ws://127.0.0.1:6800/jsonrpc").await?;
+
+        let (cmd_tx, cmd_rx) = std::sync::mpsc::channel::<RpcCommand>();
+        let (res_tx, res_rx) = std::sync::mpsc::sync_channel::<RpcResponse>(32);
+
+        spawn_worker(cmd_rx, res_tx);
 
         eframe::run_native(
                 "ADM",
                 native_opts,
-                Box::new(|_| Ok(Box::<ui::App>::default()))
+                Box::new(|_| Ok(Box::new(App::new(cmd_tx, res_rx))))
         )?;
 
         Ok(())
